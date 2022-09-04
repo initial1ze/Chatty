beec0610c8315e05926aa43f598d9d76b10f50a5
Rajesh Yadav <itsyaboiinitial1ze@gmail.com>
2022-09-04 15:41:56 +0530

    minor refactor

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,22 +1,22 @@
- //generate the message
- const generateMessage=(username,text) =>{
-    return {
-        username,
-        text,
-        createdAt : new Date().getTime()
-    }
-}
+//generate the message
+const generateMessage = (username, text) => {
+  return {
+    username,
+    text,
+    createdAt: new Date().getTime(),
+  };
+};
 
 //generate the location message
-const generateLocationMessage=(username,URL) =>{
-    return{
-        username,
-        URL,
-        createdAt:new Date().getTime()
-    }
-}
+const generateLocationMessage = (username, URL) => {
+  return {
+    username,
+    URL,
+    createdAt: new Date().getTime(),
+  };
+};
 
-module.exports={
-    generateMessage,
-    generateLocationMessage
-}
+module.exports = {
+  generateMessage,
+  generateLocationMessage,
+};
